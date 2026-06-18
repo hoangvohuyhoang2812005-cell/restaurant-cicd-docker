68f4f579ce09a2bec679562aa3418fb8e8febc04
Nguyen Vu <vuhuynguyen221205@gmail.com>
2026-06-19 00:00:24 +0700

    Thêm file cấu hình tự động deploy CI/CD p2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import AxiosDemo from './pages/AxiosDemo';
-import FormDemo from './pages/FormDemo';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import AxiosDemo from "./pages/AxiosDemo";
+import FormDemo from "./pages/FormDemo";
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
         {/* Modern glowing backdrops */}
         <div className="absolute top-0 right-[10%] w-[350px] h-[350px] bg-purple-500/5 rounded-full blur-3xl pointer-events-none"></div>
         <div className="absolute top-[40%] left-[5%] w-[400px] h-[400px] bg-indigo-500/5 rounded-full blur-3xl pointer-events-none"></div>
-        
+
         {/* Header Navigation */}
         <Navbar />
 
@@ -28,13 +28,22 @@ export default function App() {
         {/* Footer */}
         <footer className="border-t border-slate-900 bg-slate-950 py-8 text-center text-xs text-slate-500">
           <div className="max-w-6xl mx-auto px-4 flex flex-col sm:flex-row justify-between items-center gap-4">
-            <p>© {new Date().getFullYear()} Frontend Stack Boilerplate. All rights reserved.</p>
+            <p>
+              © {new Date().getFullYear()} Frontend Stack Boilerplate. All
+              rights reserved.
+            </p>
             <div className="flex gap-4">
-              <span className="hover:text-slate-355 transition-colors cursor-default">Vite</span>
+              <span className="hover:text-slate-355 transition-colors cursor-default">
+                Vite
+              </span>
               <span>•</span>
-              <span className="hover:text-slate-355 transition-colors cursor-default">React.js</span>
+              <span className="hover:text-slate-355 transition-colors ">
+                React.js
+              </span>
               <span>•</span>
-              <span className="hover:text-slate-355 transition-colors cursor-default">TailwindCSS v4</span>
+              <span className="hover:text-slate-355 transition-colors cursor-default">
+                TailwindCSS v4
+              </span>
             </div>
           </div>
         </footer>
