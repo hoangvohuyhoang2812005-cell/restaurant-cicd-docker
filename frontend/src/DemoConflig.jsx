031ae1c547703c4053909f85bc3ab8fe4018769c
Nguyen Vu <vuhuynguyen221205@gmail.com>
2026-06-30 20:02:31 +0700

    feat: tạo giao diện trên nhánh main

---
+++ frontend/src/DemoConflig.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DemoConflig = () => {
+  return (
+    <div>
+      <h1>Nguoi 1 day code</h1>
+      <h1>Nguoi 2 sua de</h1>
+    </div>
+  );
+};
+
+export default DemoConflig;
